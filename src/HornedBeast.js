55b25de177d1addfc7e2628bf2704494dd990a7e
abeershboul <abeer.shnoul@yahoo.com>
2022-08-22 21:07:30 +0300

    state

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,17 +1,46 @@
-import React from 'react';
-
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
 
 class HornedBeast extends React.Component {
-  render(){
-    return(
-    <div>
-      <h2>{this.props.title}</h2>
-      <img src={this.props.URL} alt={this.props.alt} title={this.props.imgTitle}></img>
-      <p>{this.props.Description}</p>
 
-    </div>
-    )
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+          voteNum : ""
+        }
+    }
+
+    increaseVotes = () => {
+        this.setState({
+            voteNum : this.state.voteNum + "🦄"
+        })
+    
+      }
+
+
+
+
+    render(){
+        return(
+          
+            <Card style={{ width: '18rem',display:"inline-block" }} className="beast-card">
+            <Card.Body>
+              <Card.Title>{this.props.title}</Card.Title>
+            <Card.Img variant="top" src={this.props.img} onClick={this.increaseVotes}/>
+              <Card.Text className="desc">
+              {this.props.description}
+              </Card.Text>
+              <Card.Text className="vote">
+              Votes
+              </Card.Text>
+              <Card.Text>
+              {this.state.voteNum}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        )
+    }
 }
 
 export default HornedBeast;
