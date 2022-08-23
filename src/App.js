9b4340da462f3c9e32a006e8cd93515bac7e8bc1
abeershboul <abeer.shnoul@yahoo.com>
2022-08-23 21:13:57 +0300

    passfun

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import React from "react";
-import Header from './header';
-import Main from './main'
-import Footer from './footer';
-
+import Header from "./Header";
+import Footer from "./Footer";
+import Main from "./Main";
+import data from "./data";
+import SelectedBeast from "./SelectedBeast";
 
 class App extends React.Component {
-    render() {
-        return(
-            <div>
-                <Header />
-                <div >   
-                <Main />
-                </div>
-                <Footer />
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      beastData: data,
+      displayModal: false,
+      selectedBeast: {}
+    };
+  }
+  showModal = (name) => {
+
+    const selectedBeast = data.find(beast => beast.title === name);
+    this.setState({selectedBeast, displayModal:true});
+  }
+  hideModal = () => {this.setState({displayModal: false});
+  }
+
+  
+  render() {
+    return (
+      <div>
+        <Header />
+        <div>
+          <Main
+            showModal={this.showModal}
+            beasts={this.state.beastData}
+          />
+          <SelectedBeast
+          displayModal={this.state.displayModal}
+          hideModal={this.hideModal}
+          selectedBeast={this.state.selectedBeast} />
+          
+        </div>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Header extends React.Component {
+  render(){
+    return(
+    <div>
+      <h1>HornedBeast</h1>
+      
+
+    </div>
+    )
+  }
+}
+
+export default Header;
